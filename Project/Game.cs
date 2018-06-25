f2b4fae2ac8d6daba2e265eebb969d745159148d
Brian Seward <blacktiger55@hotmail.com>
2018-06-25 09:40:32 -0600

    Update 2

--- Project/Game.cs
+++ Project/Game.cs
@@ -40,7 +40,7 @@ namespace CastleGrimtol.Project
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Clear();
             Console.WriteLine("Welcome To Runescapee");
-            Console.WriteLine("Your objective is to satisfy your hunger by defeating both the Goblin and Dragon to reach the White Castle and consuming the sacred burger");
+            Console.WriteLine("Your objective is to satisfy your hunger by navigating your way to the Bar Room, so you can get yourself a nice cold mug of ale");
             Console.WriteLine("Type 'help' to get the commands to play");
             Console.WriteLine("Your first objective is to grab the 'torch' off the wall");
 
@@ -54,8 +54,8 @@ namespace CastleGrimtol.Project
             dragonDungeon.Directions.Add("west", bar);
             bar.Directions.Add("south", equipmentRoom);
 
-            Item torch = new Item("Torch", "The Torch will light up the rooms, guide you to your destination.");
-            Item key = new Item("Key", "This key unlocks the door to the Bar room");
+            Item torch = new Item("Torch", "The Torch will light up the rooms and guide you to your destination.");
+            Item key = new Item("Key", "This key unlocks the door to the Bar Room");
 
             equipmentRoom.Items.Add(torch);
             dragonDungeon.Items.Add(key);
@@ -67,14 +67,13 @@ namespace CastleGrimtol.Project
             CurrentRoom = equipmentRoom;
         }
 
-
         public void Play()
-{
-    Setup();
-    while (Playing)
-    {
-        string selection = Console.ReadLine();
-                     switch (selection.ToLower())
+        {
+            Setup();
+            while (Playing)
+            {
+                string selection = Console.ReadLine();
+                switch (selection.ToLower())
                 {
                     case "help":
                         Console.Clear();
@@ -90,93 +89,107 @@ namespace CastleGrimtol.Project
                         break;
                     case "torch":
                         Console.Clear();
-                        UseItem(inputArray[1]);
+                        TakeItem(); //Do i need to input this in an array? 
                         break;
                     case "key":
                         Console.Clear();
-                        activeMenu = Menus.CheckoutBook;
+                        UseItem(); //Do i need to input this in an array?
                         break;
                     case "inventory":
                         Console.Clear();
                         ShowInventory();
                         break;
+                    case "look":
+                        Console.Clear();
+                        Look();
+                        break;
                     case "north":
                         Console.Clear();
                         CurrentRoom = CurrentRoom.Go("north");
+                        Console.WriteLine("You are in the Dragon's Dungeon Room, find the 'key'!");
                         break;
                     case "south":
                         Console.Clear();
                         CurrentRoom = CurrentRoom.Go("south");
+                        Console.WriteLine("You are in the Equipment Room, find a 'torch'!");
                         break;
                     case "east":
                         Console.Clear();
                         CurrentRoom = CurrentRoom.Go("east");
+                        Console.WriteLine("You are in the Goblin's Lair, he may be on vacation?");
                         break;
                     case "west":
                         Console.Clear();
                         CurrentRoom = CurrentRoom.Go("west");
+                        Console.WriteLine("You are now in the bar room! Have a good time brotha!");
                         break;
-                    default:
-                        if (activeMenu.Equals(Menus.CheckoutBook))
-                        {
-                            myLibrary.Checkout(selection);
-                        }
-                        else
-                        {
-                            myLibrary.ReturnBook(selection);
-                        }
-                        break;
+                    // default:
+                    //     if (activeMenu.Equals(Menus.CheckoutBook))
+                    //     {
+                    //         myLibrary.Checkout(selection);
+                    //     }
+                    //     else
+                    //     {
+                    //         myLibrary.ReturnBook(selection);
+                    //     }
+                    //     break;
                 }
-    
-    }
-}
+            }
+        }
+
+        public void Look()
+        {
+            // CurrentRoom  -- Need to print your description when user types "look".
+        }
+        public void ShowInventory()
+        {
+            Console.WriteLine("Items you have in your inventory:");
+            CurrentPlayer.Inventory.ForEach(item =>
+            {
+                Console.WriteLine(item.Name);
+            });
+        }
+        public void UseItem(Item item)
+        {
+            if (item.Name == "key")
+            {
+                Console.WriteLine("You have found the key to unlock the door to the Bar Room!");
+            }
+            else
+            {
+                Console.WriteLine("You need to find the 'key' to unlock the final door, which leads you into a Bar");
+            }
+        }
+
+        // TODOS: 1. ELSE STATEMENT FOR WHEN USER FAILS TO USE TORCH IN DRAGON ROOM, ALIVE = FALSE; RESTART();
+        //        2. FIX PROGRAM.CS TO RUN GAME, SOLVE WHY IGAME IS HIGHLIGHTED ON THIS PAGE... ASK INSTRUCTORS.
+        //        3. ASK INSTRUCTORS IF I NEED TO REPLACE THE ROOM DESCRIPTION WHEN USING AN ITEM? OR IS CONSOLE.WRITELINE WHEN ITEM IS USED SUFFICE REQ.
+        //        4. FIX TAKE/USE ITEM IN SWITCH STATEMENT.
 
 
-public void ShowInventory()
-{
-    Console.WriteLine("Items you have in your inventory:");
-    CurrentPlayer.Inventory.ForEach(item =>
-    {
-        Console.WriteLine(item.Name);
-    });
-}
-public void UseItem(Item item)
-{
-    if (item.Name == "key")
-    {
-        Console.WriteLine("You have found the key to unlock the door to the Bar Room!");
-    }
-    else
-    {
-        Console.WriteLine("You need to find the 'key' to unlock the final door, which leads you into a Bar");
-    }
-}
-
-    
 
 
-        // NEED TO WRITE SWITCHES 
+
+
 
         // Console.WriteLine("Type 'east' to enter the Goblin's Lair");
         // Console.WriteLine("Type 'north' to enter the Dragons Dungeon");
         // Console.WriteLine("Type 'west' to enter the White Castle Room");
         // Console.WriteLine("Type 'south' to enter the Equipment Room");
-        
+
         // Console.WriteLine("You light the torch. A dragon appears in front of you. He sees you, but identifies you as a friend because you carry the torch. You see a key lying next to him.");
         // Console.WriteLine("You do not have a torch, and had wandered your way into the Dragons Dungeon. The dragon does not see you as a friend and spits fire at you. You die.");
 
         // Console.WriteLine("You enter the White Castle room, and a greeted by winches, serving burgers and beer. You win!");
-        
-
 
         // Console.WriteLine("Type 'Attack' or 'Pet' to make your move on the Great White Dragon")
         // Console.WriteLine("You have slayed the Great White Dragon")
         // Console.Write("Type 'Ride' to ride the dragon up to White Castle or 'Drop' to fight the skinless Goblin once again");`  \
-        
+
         // Console.Writeline("Type 'eat' to satisfy your hunger");
         // Console.Writeline("You have consumed the royal burger and are now declared 'Burger King'");
         // //if (CurrentRoom.Items.Count > 0) // for the torch... do switch statements for directions. IF they type a command, .. north, south, east, west.. switch statements.
 
-        }
     }
+}
 
